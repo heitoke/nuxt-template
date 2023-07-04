69af4ea79199991afd48b7966f9b83d15c3b7348
heitoke <me@heito.xyz>
2023-07-04 17:01:58 +0300

    Added new server route (blogs)

---
+++ server/utils/blogs.ts
@@ -0,0 +1,14 @@
+import { IBlog } from "../../types/blog";
+
+const blogs: Array<IBlog> = [
+    {
+        name: 'first',
+        title: 'First blog'
+    },
+    {
+        name: 'fire',
+        title: 'Fire'
+    }
+];
+
+export default blogs;
---
+++ types/blog.ts
@@ -0,0 +1,6 @@
+export interface IBlog {
+    name: string;
+    title: string;
+    image?: string;
+    description?: string;
+}
